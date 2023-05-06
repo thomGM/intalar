4f300d8e60d10e978b21488d0b07c863fcaacd56
thomGM <milkewicz432@gmail.com>
2023-05-06 05:27:32 -0300

    modifiquei a conexao da tabela e criei a tabela clientes

--- database/migrations/2023_05_06_082222_create_clientes_table.php
+++ database/migrations/2023_05_06_082222_create_clientes_table.php
@@ -4,15 +4,16 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateClientesTable extends Migration
+return new class extends Migration
 {
     /**
      * Run the migrations.
      */
-    public function up()
+    public function up(): void
     {
         Schema::create('clientes', function (Blueprint $table) {
             $table->id();
+            $table->timestamps();
             $table->date('aniversario');
             $table->string('nome');
             $table->string('cpf_cnpj');
@@ -25,5 +26,12 @@ class CreateClientesTable extends Migration
             $table->string('cidade'); 
         });
     }
-}
 
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('clientes');
+    }
+};
